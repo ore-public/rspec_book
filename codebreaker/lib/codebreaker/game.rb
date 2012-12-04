9cfcef70e56fc22f94997d863584d3995b276f0a
Masaya Konishi <konishi@office-ore.net>
2012-12-04 20:12:25 +0900

    guessを綺麗にする

--- codebreaker/lib/codebreaker/game.rb
+++ codebreaker/lib/codebreaker/game.rb
@@ -12,7 +12,7 @@ module Codebreaker
 
     def guess(guess)
       marker = Marker.new(@secret, guess)
-      @output.puts '+' * marker.exact_match_count(guess) + '-' * marker.number_match_count(guess)
+      @output.puts '+' * marker.exact_match_count + '-' * marker.number_match_count
     end
 
     class Marker
@@ -20,24 +20,24 @@ module Codebreaker
         @secret, @guess = secret, guess
       end
 
-      def number_match_count(guess)
+      def number_match_count
         (0..3).inject(0) do |count, index|
-          count + (number_match?(guess, index) ? 1 : 0)
+          count + (number_match?(index) ? 1 : 0)
         end
       end
 
-      def exact_match_count(guess)
+      def exact_match_count
         (0..3).inject(0) do |count, index|
-          count + (exact_match?(guess, index) ? 1 : 0)
+          count + (exact_match?(index) ? 1 : 0)
         end
       end
 
-      def exact_match?(guess, index)
-        guess[index] == @secret[index]
+      def exact_match?(index)
+        @guess[index] == @secret[index]
       end
 
-      def number_match?(guess, index)
-        @secret.include?(guess[index]) && !exact_match?(guess, index)
+      def number_match?(index)
+        @secret.include?(@guess[index]) && !exact_match?(index)
       end
     end
   end
