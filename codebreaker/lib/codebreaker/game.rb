9db522d47a5bce31b1faf11798fa77215fe8d008
Masaya Konishi <konishi@office-ore.net>
2012-12-04 19:50:50 +0900

    number_matchも数えるという意味を持たせてリファクタリング

--- codebreaker/lib/codebreaker/game.rb
+++ codebreaker/lib/codebreaker/game.rb
@@ -12,7 +12,7 @@ module Codebreaker
 
     def guess(guess)
       exact_match_count = 0
-      mark = ''
+      number_match_count = 0
       (0..3).each do |index|
         if exact_match?(guess, index)
           exact_match_count += 1
@@ -20,10 +20,10 @@ module Codebreaker
       end
       (0..3).each do |index|
         if number_match?(guess, index)
-          mark << '-'
+          number_match_count += 1
         end
       end
-      @output.puts '+' * exact_match_count + mark
+      @output.puts '+' * exact_match_count + '-' * number_match_count
     end
 
     def exact_match?(guess, index)
