9226076e5b8d760b878bb4a53d6c1ec3f4668715
Masaya Konishi <konishi@office-ore.net>
2012-12-04 19:54:36 +0900

    引数を抽出したメソッドに引き継ぐ

--- codebreaker/lib/codebreaker/game.rb
+++ codebreaker/lib/codebreaker/game.rb
@@ -11,19 +11,23 @@ module Codebreaker
     end
 
     def guess(guess)
-      exact_match_count = 0
       number_match_count = 0
-      (0..3).each do |index|
-        if exact_match?(guess, index)
-          exact_match_count += 1
-        end
-      end
       (0..3).each do |index|
         if number_match?(guess, index)
           number_match_count += 1
         end
       end
-      @output.puts '+' * exact_match_count + '-' * number_match_count
+      @output.puts '+' * exact_match_count(guess) + '-' * number_match_count
+    end
+
+    def exact_match_count(guess)
+      exact_match_count = 0
+      (0..3).each do |index|
+        if exact_match?(guess, index)
+          exact_match_count += 1
+        end
+      end
+      exact_match_count
     end
 
     def exact_match?(guess, index)
@@ -33,5 +37,6 @@ module Codebreaker
     def number_match?(guess, index)
       @secret.include?(guess[index]) && !exact_match?(guess, index)
     end
+
   end
 end
